050a169ce9ffc18f1b038297b752a7e11d79d81d
Bùi Hữu Thắng <thang.bui@adamodigital.com>
2021-09-09 10:39:05 +0700

    Revert "Merge branch 'develop_minh_43993' into 'master'"
    This reverts merge request !3

--- resources/js/display_navbar.js
+++
@@ -1,9 +0,0 @@
-var nav = document.querySelector("js-navbar");
-
-window.addEventListener("scroll", function () {
-    if (window.pageYOffset > 0) {
-        nav.classList.add("bg-dark", "shadow");
-    } else {
-        nav.classList.remove("bg-dark", "shadow");
-    }
-});

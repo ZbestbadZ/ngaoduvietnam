553300c1742ca84a74b873e45da79a7aadbf1767
phanhoang134 <phanhoang134@gmail.com>
2021-09-13 07:53:11 +0700

    link at sidebar

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -1,8 +1,8 @@
 <ul class="sidebar-nav">
     <li><a href="#">Dashboard</a></li>
-    <li><a href="#">Category Tour Management</a></li>
+    <li><a href="{{ route('categories.index') }}">Category Tour Management</a></li>
     <li><a href="#">Destination Tour Management</a></li>
-    <li><a href="#">Tour Management</a></li>
+    <li><a href="{{ route('tours.index') }}">Tour Management</a></li>
     <li><a href="#">Booking Management</a></li>
     <li><a href="#">Setting</a></li>
 </ul>
